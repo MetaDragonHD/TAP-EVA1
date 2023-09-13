9000d6de93b9b288c35edb5d8eaff3e40afc13aa
MetaDragonHD <118222449+MetaDragonHD@users.noreply.github.com>
2023-09-12 17:50:23 -0700

    Add files via upload

---
+++ EVA1_7_stack/src/eva1_7_stack/EVA1_7_stack.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package eva1_7_stack;
+
+/*--> Sergio Eduardo Lastiri Torres - 22550031 <--*/
+
+public class EVA1_7_stack
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Inicio Main");
+        A();
+        System.out.println("Fin main");
+    }
+    
+    public static void A()
+    {
+        System.out.println("Inicio A");
+        B();
+        System.out.println("Fin A");
+    }
+    
+    public static void B()
+    {
+        System.out.println("Inicio B");
+        System.out.println("Fin B");
+    }
+    
+}
