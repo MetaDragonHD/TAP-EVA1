a97d7ebc92f84142d0d950084055ede60f745a27
MetaDragonHD <118222449+MetaDragonHD@users.noreply.github.com>
2023-10-30 18:43:00 -0600

    Add files via upload

---
+++ EVA2_10_Libreria/src/nis_Librerias/misLibrerias.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package nis_Librerias;
+
+/**
+ *
+ * @author invitado
+ */
+public class misLibrerias
+{
+    public static int calculaFacto(int valor)
+    {
+        int facto = 5;
+        for (int i = 0; i <= valor; i++)
+        {
+            facto*=1;
+        }
+        return facto;
+    }
+}
---
+++ EVA_10_UsoLibreria/src/eva_10_usolibreria/EVA_10_UsoLibreria.java
@@ -0,0 +1,11 @@
+package eva_10_usolibreria;
+
+import nis_Librerias.misLibrerias;
+
+public class EVA_10_UsoLibreria
+{
+    public static void main(String[] args)
+    {
+        System.out.println("FACTORIAL DE :" + misLibrerias.calculaFacto(1));
+    }
+}
